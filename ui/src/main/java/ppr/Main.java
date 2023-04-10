7ae9fe017d283ccda3490e4f154ffa132b9b84ce
Balint Taschner <balint.taschner@siemens.com>
2023-04-10 16:50:08 +0200

    Made buttons work, added UI to benchmark mode

--- ui/src/main/java/ppr/Main.java
+++ ui/src/main/java/ppr/Main.java
@@ -3,9 +3,7 @@ package ppr;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -30,18 +28,32 @@ import java.util.stream.Collectors;
  * @version 2023-03-24
  */
 public class Main  extends  Application{
-    private static int size = 10;
+    private  static final boolean benchmarkMode = false;
+    private static final int size = 10;
+    private static final int width = 600;
+    private static final int height = 600;
     private static int generationCount = 0;
-    private static GridPane grid = new GridPane();
+    private static final GridPane grid = new GridPane();
     private static Label generationLabel = new Label();
-    private static int width = 600;
-    private static int height = 600;
+    private static PrintWriter out;
+    private static StartMessage start;
+    private static Button startButton;
+
+    private static String getStartJson() {
+        var objectMapper = new ObjectMapper();
+        try {
+            String startJson = objectMapper.writeValueAsString(start);
+            System.out.println(startJson);
+            System.out.println();
+            return startJson;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return "";
+    }
 
     public static void main(String[] args) {
 
-        StartMessage start;
-        String startJson = "";
-        var objectMapper = new ObjectMapper();
         {
             var textInput = ".........." +
                     "...**....." +
@@ -69,10 +81,12 @@ public class Main  extends  Application{
             // * value not null => BENCHMARK
             //
             // =================
-            // var benchmarkInput = new BenchmarkInput();
-            // benchmarkInput.generations = 10;
-            // benchmarkInput.iterations = 10;
-            // start.benchmarkInput = benchmarkInput;
+            if(benchmarkMode) {
+             var benchmarkInput = new BenchmarkInput();
+             benchmarkInput.generations = 10;
+             benchmarkInput.iterations = 10;
+             start.benchmarkInput = benchmarkInput;
+            }
 
             // Customize thread config for server
             // var threadConfig = new ThreadConfig();
@@ -80,33 +94,27 @@ public class Main  extends  Application{
             // threadConfig.threadCount = 1;
             // start.threadConfig = threadConfig;
 
-            try {
-                startJson = objectMapper.writeValueAsString(start);
-                System.out.println(startJson);
-                System.out.println();
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
         }
 
 
         String hostname = "localhost";
         int portNumber = 1234;
-        var grid = start.input.grid;
         try (
                 var socket = new Socket(hostname, portNumber);
                 var out = new PrintWriter(socket.getOutputStream(), true);
                 var in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
         ) {
+            Main.out = out;
             // TODO: exit? error handling?
 
-            if (start.benchmarkInput == null) {
-                Thread launchThread = new Thread(() -> launch(args));
-                launchThread.start();
-                interactive(startJson, grid, 10, out, in);
+            Thread launchThread = new Thread(() -> launch(args));
+            launchThread.start();
+            if (!benchmarkMode) {
+                interactive(in);
+            } else {
+                generationLabel.setText("Generations: " + start.benchmarkInput.generations+"\n Iterations: "+start.benchmarkInput.iterations);
+                benchmark(in);
             }
-            else
-                benchmark(startJson, grid, out, in);
 
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -114,17 +122,19 @@ public class Main  extends  Application{
     }
 
     public static void interactive(
-        String startJson,
-        List<List<Boolean>> grid,
-        int generations,
-        PrintWriter out,
         BufferedReader in
     ) throws IOException, InterruptedException {
+        int generations = start.input.generations;
         var objectMapper = new ObjectMapper();
-
-        out.println(startJson);
+        var grid = start.input.grid;
+        start.input.waitUntilManualOrNextIsTrue();
+        out.println(getStartJson());
 
         for (var i = 0; i < generations; ++i) {
+            start.input.waitUntilManualOrNextIsTrue();
+            if(start.input.getEndProgram()) {
+                break;
+            }
             // Get diff
             var updateJson = in.readLine();
             var update = objectMapper.readValue(updateJson, UpdateMessage.class);
@@ -134,7 +144,10 @@ public class Main  extends  Application{
             String gridString = gridString(grid);
             createChessboard(grid);
             Platform.runLater(() -> {
-                generationLabel.setText("Generation: " + update.generation);
+                generationCount = update.generation;
+                if(!benchmarkMode) {
+                    generationLabel.setText("Generation: " + update.generation);
+                }
             });
             System.out.println(update.generation);
             System.out.println(gridString);
@@ -144,6 +157,12 @@ public class Main  extends  Application{
 
             // Send if processing should continue or not
             boolean next = (i + 1) != generations;
+
+            if(start.input.getNext()) {
+                next = true;
+                start.input.setNext(false);
+            }
+
             var ackMessage = new UpdateAckMessage(next);
             var ackMessageJson = objectMapper.writeValueAsString(ackMessage);
             out.println(ackMessageJson);
@@ -151,14 +170,12 @@ public class Main  extends  Application{
     }
 
     public static void benchmark(
-            String startJson,
-            List<List<Boolean>> grid,
-            PrintWriter out,
             BufferedReader in
     ) throws IOException, InterruptedException {
         var objectMapper = new ObjectMapper();
 
-        out.println(startJson);
+        start.input.waitUntilManualOrNextIsTrue();
+        out.println(getStartJson());
 
         var resultJson = in.readLine();
         var result = objectMapper.readValue(resultJson, BenchmarkMessage.class);
@@ -166,6 +183,9 @@ public class Main  extends  Application{
         System.out.println("Average time: " + result.averageTime + " ms");
         System.out.println("Min time: " + result.minTime + " ms");
         System.out.println("Max time: " + result.maxTime + " ms");
+
+        Platform.runLater(() -> generationLabel.setText("Average time: " + result.averageTime + " ms\nMin time: " + result.minTime + " ms\nMax time: " + result.maxTime + " ms"));
+        createChessboard(result.grid);
         String gridString = gridString(result.grid);
         System.out.println(gridString);
     }
@@ -186,27 +206,27 @@ public class Main  extends  Application{
     @Override
     public void start(Stage primaryStage) {
         // Create the chessboard
-        createChessboard(null);
+        createChessboard(start.input.grid);
 
         // Create the Start and Next buttons
-        Button startButton = new Button("Start");
+        startButton = new Button("Start");
         Button nextButton = new Button("Next");
         startButton.setOnAction(e -> handleStartButtonClick());
         nextButton.setOnAction(e -> handleNextButtonClick());
-
-        // Create the generation counter label
-        generationLabel = new Label("Generation: " + generationCount);
 
         // Create the layout
         GridPane layout = new GridPane();
         layout.setAlignment(Pos.CENTER);
         layout.add(grid, 0, 0, 3, 1);
         layout.add(startButton, 0, 1);
-        layout.add(nextButton, 1, 1);
+        if(!benchmarkMode) {
+            layout.add(nextButton, 1, 1);
+        }
         layout.add(generationLabel, 2, 1);
 
         // Create the scene and show the stage
         Scene scene = new Scene(layout, width, height);
+        primaryStage.setOnCloseRequest(event -> start.input.setEnd());
         primaryStage.setTitle("Game Of Life");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -214,13 +234,10 @@ public class Main  extends  Application{
 
     private static void createChessboard(List<List<Boolean>> input) {
         Platform.runLater(() -> {
+            start.input.grid = input;
             grid.getChildren().clear();
         });
-        boolean isEmpty = false;
-        if(input == null || input.isEmpty()) {
-            isEmpty = true;
-        } else {
-        }
+        boolean isEmpty = input == null || input.isEmpty();
         int rowSize = isEmpty ? size : input.size();
         int recWidth = (width-200)/rowSize;
         for (int row = 0; row < rowSize; row++) {
@@ -240,25 +257,35 @@ public class Main  extends  Application{
                 final int x = col;
                 final int y = row;
                 rect.setOnMouseClicked(e -> handleChessboardClick(x, y));
-                Platform.runLater(() -> {
-                    grid.add(rect, x, y);
-                });
+                Platform.runLater(() -> grid.add(rect, x, y));
             }
         }
     }
 
     private static void handleChessboardClick(int x, int y) {
+        if(generationCount > 0) {
+            return;
+        }
         // Handle the click on the chessboard
-        System.out.println("Clicked on cell " + x + "," + y);
+        start.input.grid.get(y).set(x, !start.input.grid.get(y).get(x));
+        createChessboard(start.input.grid);
     }
 
     private static void handleStartButtonClick() {
         // Handle the click on the Start button
         System.out.println("Start button clicked");
+        boolean newManual = !start.input.getManual();
+        start.input.setManual(newManual);
+        startButton.setText(newManual ? "Start" : "Pause");
+        if(start.benchmarkInput != null) {
+            generationLabel.setText("Starting benchmarking.");
+            startButton.setVisible(false);
+        }
     }
 
     private static void handleNextButtonClick() {
         // Handle the click on the Next button
         System.out.println("Next button clicked");
+        start.input.setNext(true);
     }
 }
--- ui/src/main/java/ppr/messages/GameInput.java
+++ ui/src/main/java/ppr/messages/GameInput.java
@@ -11,4 +11,40 @@ import java.util.List;
 public class GameInput {
     public int size;
     public List<List<Boolean>> grid;
+
+    public int generations = 999;
+
+    private boolean isManual = true;
+
+    private boolean triggerNext = false;
+    private boolean endProgram = false;
+    public synchronized void setEnd() {
+        this.endProgram = true;
+        notifyAll(); // notify waiting threads that the value has changed
+    }
+    public  synchronized  boolean getEndProgram() {
+        return this.endProgram;
+    }
+    public synchronized void setManual(boolean value) {
+        this.isManual = value;
+        notifyAll(); // notify waiting threads that the value has changed
+    }
+
+    public synchronized boolean getManual() {
+        return this.isManual;
+    }
+    public synchronized void setNext(boolean value) {
+        this.triggerNext = value;
+        notifyAll(); // notify waiting threads that the value has changed
+    }
+
+    public synchronized boolean getNext() {
+        return this.triggerNext;
+    }
+
+    public synchronized void waitUntilManualOrNextIsTrue() throws InterruptedException {
+        while (this.isManual && !this.triggerNext && !endProgram) {
+            wait(); // wait until flag is true
+        }
+    }
 }
